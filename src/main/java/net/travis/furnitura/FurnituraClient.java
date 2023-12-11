abe5f92376e63566e558afc209aab5d7e3ae2896
TheGuyTravis <travisjamesza@gmail.com>
2023-12-11 19:29:38 +0200

    Initial commit

---
+++ src/main/java/net/travis/furnitura/FurnituraClient.java
@@ -0,0 +1,10 @@
+package net.travis.furnitura;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FurnituraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/travis/furnitura/FurnituraDataGenerator.java
@@ -0,0 +1,11 @@
+package net.travis.furnitura;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FurnituraDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
